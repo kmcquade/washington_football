102128c67faa5acc9dbb99e3adcde811c3169b63
Kinnaird McQuade <kmcquade@salesforce.com>
2019-11-13 18:19:09 -0500

    Bruh

--- tasks.py
+++ tasks.py
@@ -1,17 +1,12 @@
 #! /bin/env python3
 
 from invoke import task, Collection
-from washington_football.command.suck import suck
+# from washington_football.command.suck import suck
 
 # Create the necessary collections (namespaces)
 ns = Collection()
 test = Collection('test')
 ns.add_collection(test)
-
-
-@task()
-def reminder():
-    suck(True)
 
 
 @task
@@ -31,7 +26,6 @@ def run_tests(c):
     c.run('nosetests -v')
 
 
-test.add_task(reminder, 'reminder')
 test.add_task(security_scan, 'security')
 test.add_task(run_linter, 'lint')
 test.add_task(run_tests, 'unit')
